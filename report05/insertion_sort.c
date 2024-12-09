480a81a7085883446d36d42ddf3aaeeb3551dd70
Ryoga-exe <git@ryoga.dev>
2024-12-09 10:08:14 +0900

    feat: report05, Q2

---
+++ report05/insertion_sort.c
@@ -0,0 +1,14 @@
+#include "insertion_sort.h"
+
+void insertion_sort(int a[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = a[i];
+        int j = i - 1;
+
+        while (j >= 0 && a[j] > key) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+}
---
+++ report05/main_insertion_sort.testcase.c
@@ -0,0 +1,36 @@
+#include "insertion_sort.h"
+#include "sort_util.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(void) {
+    srand(time(NULL));
+
+    for (int test = 0; test < 5; test++) {
+        int n = rand() % 6 + 5;
+        int *a = (int *)calloc(n, sizeof(int));
+
+        for (int i = 0; i < n; i++) {
+            a[i] = rand() % 100;
+        }
+
+        insertion_sort(a, n);
+
+        if (!is_sorted(a, n)) {
+            fprintf(stderr,
+                    "Hey! Your sorting program seems to be broken :(\n");
+            exit(EXIT_FAILURE);
+        }
+
+        free(a);
+    }
+
+    int manual_array[] = {64, 25, 12, 22, 11};
+    int manual_n = sizeof(manual_array) / sizeof(manual_array[0]);
+
+    display(manual_array, manual_n);
+    insertion_sort(manual_array, manual_n);
+    display(manual_array, manual_n);
+}
