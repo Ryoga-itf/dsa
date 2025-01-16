5edcb60347a01896dfe585e8ea009b1a6cd83c54
Ryoga-exe <git@ryoga.dev>
2025-01-16 15:41:27 +0900

    feat: init report07

---
+++ report07/cmp.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+extern bool isVerbose;
+extern int Ncmp;
+
+/**
+ * 文字比較関数．比較回数をカウントする．
+ * @param a 比較文字
+ * @param b 比較文字
+ * @return a と b が等しければ true, 等しくなければ false.
+ */
+bool cmp(char a, char b) {
+    if (isVerbose)
+        fprintf(stderr, "cmp(%c, %c)\n", a, b);
+    Ncmp++;
+    if (a == b)
+        return true;
+    else
+        return false;
+}
---
+++ report07/mainKMP.c
@@ -0,0 +1,78 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PAT_MAX 256               // パターンの最大の長さ
+#define TEXT_MAX 1024 * 1024 * 10 // テキストの最大の長さ（10 MB）
+
+bool isVerbose = false; // 比較回数表示スイッチ
+unsigned int Ncmp = 0;  // 比較回数のカウンタ
+
+int readFile(char *, char *);
+bool cmp(char, char);
+
+/**
+ * next配列の計算
+ * @param pat パターン配列
+ * @param next next配列
+ * @return なし
+ */
+void compnext(char *pat, unsigned int *next) {
+    // 関数を完成させる．
+    return;
+}
+
+/**
+ * KMP法による文字列照合．
+ * @param text テキスト
+ * @param pat 検索パターン
+ * @return 照合に成功した位置．失敗した場合は -1．
+ */
+int kmp(char *text, unsigned int textlen, char *pat, unsigned int patlen) {
+    unsigned int next[PAT_MAX]; // NEXT配列
+                                // 関数を完成させる
+}
+
+/**
+ * メイン関数
+ * @param argc
+ * @param argv
+ * @return 終了ステータス：正常終了 0 / それ以外 1
+ */
+int main(int argc, char **argv) {
+    char pat[PAT_MAX]; // 検索パターン
+    char *text = (char *)malloc(TEXT_MAX * sizeof(char));
+    int patlen = 0;
+    int textlen = 0;
+
+    // 引数処理・ファイル読み込み
+    if (argc < 3 || 4 < argc) {
+        fprintf(stderr, "Usage: ./main [-v] <text file> <pattern file>\n");
+        return 1;
+    }
+    int i = 1;
+    if (strcmp(argv[i], "-v") == 0) {
+        isVerbose = true;
+        i++;
+    }
+    textlen = readFile(argv[i++], text);
+    patlen = readFile(argv[i], pat);
+
+    // pat 末尾の改行コードを削除
+    while (pat[patlen - 1] == '\r' || pat[patlen - 1] == '\n')
+        pat[--patlen] = '\0';
+
+    // 文字列照合・結果表示
+    if (isVerbose) {
+        printf("text size: %d\n", textlen);
+        printf("pattern size: %d\n", patlen);
+    }
+    printf("Pattern found at %d.\n", kmp(text, textlen, pat, patlen));
+    if (isVerbose)
+        printf("# of comparison(s): %d.\n", Ncmp);
+
+    free(text);
+
+    return 0;
+}
